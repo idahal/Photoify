7fa61950488e4912bdc759e8dcf1c04dfa2d7536
Ida Hallner <bee_ida@hotmail.com>
2018-12-12 18:34:49 +0100

    add toggle function mobile page

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// navbar
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
