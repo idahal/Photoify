6801f5e7bbc192e8144d3bf9802de1b17a9cf935
Ida Hallner <bee_ida@hotmail.com>
2018-12-12 18:35:53 +0100

    add navbar with links

---
+++ views/navigation.php
@@ -0,0 +1,25 @@
+<nav class="navbar">
+      <span class="navbar-toggle" id="js-navbar-toggle">
+          <svg width="36" height="24" viewBox="0 0 36 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="36" height="3.0" rx="1.8" fill="#ffffff"/>
+          <rect y="9.6001" width="36" height="3.0" rx="1.8" fill="#ffffff"/>
+          <rect y="20.3999" width="36" height="3.0" rx="1.8" fill="#ffffff"/>
+        </svg>
+      </span>
+<a href="#" class="logo">Photoify</a>
+        <ul class="main-nav" id="js-menu">
+            <li>
+                <a class="nav-link" href="/index.php">Home</a>
+            </li>
+            <li>
+                 <a class="nav-link" href="/about.php">About</a>
+            </li>
+            <li>
+                <?php if (isset($_SESSION['user'])): ?>
+                    <a class="nav-link" href="app\users\logout.php">Logout</a>
+                <?php else: ?>
+                    <a class="nav-link" href="/login.php">Login</a>
+                <?php endif; ?>
+            </li>
+        </ul>
+</nav>
