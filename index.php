e5a591afddfedac7318a77886a8d63bc5b5a759c
Ida Hallner <bee_ida@hotmail.com>
2018-12-17 07:59:28 +0100

    moved  data to start page

--- index.php
+++ index.php
@@ -1,40 +1,20 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article class="homepage">
-    <!-- <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p> -->
-    <form class="create-acount" action="/app/users/signup.php" method="post">
-        <p>
-          <label>User name</label><br>
-          <input type="text" name="username">
-        </p>
-      <p>
-        <label>First name</label><br>
-        <input type="text" name="first_name">
-      </p>
-      <p>
-        <label>Last name</label><br>
-        <input type="text" name="last_name">
-      </p>
-      <p>
-        <label>Email</label><br>
-        <input type="email" name="email" required>
-      </p>
-      <p>
-        <label>Password</label><br>
-        <input type="password" name="password">
-      </p>
-      <p>
-        <button class="sign-up">Sign up</button>
-     </p>
-    </form>
+<article>
 
+    <?php
+    // If user is logged in
+    if(isset($_SESSION['user'])):  ?>
 
+    <?php require __DIR__.'/home.php'; ?>
 
-<!-- if the user is logged in create a welcome message -->
-    <?php if (isset($_SESSION['user'])): ?>
-       <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
-   <?php endif; ?>
+    <?php
+    //If user is not logged in
+    else: ?>
+        <?php require __DIR__.'/start.php'; ?>
+    <?php endif; ?>
 </article>
+
+<!-- <?php unset($_SESSION['messages']); ?> -->
 
 <?php require __DIR__.'/views/footer.php'; ?>
