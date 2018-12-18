2f361249d87589d87136c30bdaa5bd8004ef845c
Ida Hallner <bee_ida@hotmail.com>
2018-12-18 13:24:55 +0100

    change database path

--- app/config.php
+++ app/config.php
@@ -6,5 +6,5 @@ declare(strict_types=1);
 
 return [
     'title' => 'Photoify',
-    'database_path' => sprintf('sqlite:%s/database/photoify.db', __DIR__),
+    'database_path' => sprintf('sqlite:%s/database/photoify2.db', __DIR__),
 ];
