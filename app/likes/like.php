f60a508c122c1da2b6630e17bbb0203f436e8f77
Ida Hallner <bee_ida@hotmail.com>
2019-01-04 14:34:54 +0100

    add like function and print likes on homepage

--- app/likes/like.php
+++ app/likes/like.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-if (isset($_POST['like'], $_POST['post_id'])) {
-    // $id = $_SESSION['user']['user_id'];
-    $like = (int) $_POST['like'];
+if (isset($_POST['post_id'])) {
+    $id = $_SESSION['user']['user_id'];
     $postId = $_POST['post_id'];
 
-    $statement = $pdo->prepare('UPDATE likes SET like = :like +1 WHERE post_id = :post_id');
-    die(var_dump($like));
+// Check if like exist in database
+ $statement = $pdo->prepare('SELECT * FROM likes WHERE post_id = :post_id AND user_id = :user_id');
+ $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+ $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
+ $statement->execute();
+ $alreadyLiked = $statement->fetch(PDO::FETCH_ASSOC);
+
+ // if alreadyLiked is false put the like in database
+if (!$alreadyLiked) {
+    $statement = $pdo->prepare(
+   'INSERT INTO likes (post_id, user_id)
+    VALUES (:post_id, :user_id);');
 
     if (!$statement)
         {
@@ -18,31 +27,10 @@ if (isset($_POST['like'], $_POST['post_id'])) {
         }
 
         // binds variables to parameteres for insert statement
-            $statement->bindParam(':like', $like, PDO::PARAM_INT);
             $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
-
+            $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
             $statement->execute();
-            redirect('/');
 
+        }
 }
-
-// if (isset($_SESSION['user'], $_GET['type'], $_GET['post_id'])) {
-//     $userId = $_SESSION['user']['user_id'];
-//     $like = $_GET['type'];
-//     $postId = $_GET['post_id'];
-//
-//     $statement = $pdo->prepare('UPDATE likes SET like = :like WHERE user_id = :user_id AND post_id = :post_id');
-//     if (!$statement)
-//     {
-//         die(var_dump($pdo->errorInfo()));
-//     }
-//
-//     // binds variables to parameteres for insert statement
-//     $statement->bindParam(':like', $like, PDO::PARAM_INT);
-//     $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
-//     $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
-//
-//     $statement->execute();
-//     redirect('/home.php');
-//
-// }
+redirect('/');
--- home.php
+++ home.php
@@ -1,39 +1,39 @@
 
 <article class="homepage">
-    <!-- <h1><?php echo $config['title']; ?></h1> -->
-
 
     <div class="top-gallery-page">
-<!-- if the user is logged in create a welcome message -->
-    <?php if (isset($_SESSION['user'])): ?>
-       <img class="profile-pic" src="image/profile/<?php echo $_SESSION['user']['profile_pic'];?>" alt="avatar">
-    <?php endif; ?>
-<br><br><br>
-    <form action="app/posts/store.php" method="post" enctype="multipart/form-data">
-             <div>
-                 <p><label for="images">Upload image</label></p>
-                 <input type="file"  value="upload file" name="post" id="images" accept=".png, .jpeg, .jpg" multiple required>
-             </div><br>
-             <div class="form-group">
-                 <p>
-                     <label>Add text</label><br>
-                     <input type="text" name="content">
-                 </p>
-             </div>
-             <button class="upload-profile-pic" type="submit">Upload</button>
-         </form>
-         <br>
-     </div>
+<!-- if the user is logged in show profile pic -->
+        <?php if (isset($_SESSION['user'])): ?>
+            <img class="profile-pic-home" src="image/profile/<?php echo $_SESSION['user']['profile_pic'];?>" alt="avatar">
+        <?php endif; ?>
 
+        <!-- upload photo -->
+        <div class="upload-btn-wrapper">
+            <form action="app/posts/store.php" method="post" enctype="multipart/form-data">
+                 <p><label for="images">Upload image</label></p>
+                 <input class="btn" type="file"  value="upload file" name="post" id="images" accept=".png, .jpeg, .jpg" multiple required>
+                    <div class="form-group">
+                        <p>
+                            <input type="text" name="content" placeholder="Add text to your photo">
+                        </p>
+                    </div>
+                    <button class="upload-photo" type="submit">Upload</button>
+             </form>
+         </div><br>
+     </div>
 <?php
-// join users column with posts column. Print post, name and comment.
-    $statement = $pdo->prepare('SELECT a.post, a.content, a.post_id, c.first_name, c.last_name, c.profile_pic FROM posts a
-        LEFT JOIN users c ON a.user_id=c.user_id ORDER BY a.date DESC;');
+    // join users column with posts column. Print post, name and comment.
+    $statement = $pdo->prepare(
+        'SELECT a.post, a.content, a.post_id, c.first_name, c.last_name, c.profile_pic, count(l.post_like)
+        AS "like" FROM posts a
+        LEFT JOIN users c ON a.user_id=c.user_id
+		LEFT JOIN likes l ON a.post_id=l.post_id
+		GROUP BY a.post_id
+		ORDER BY a.date DESC;');
         $statement->execute();
         $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-        // die(var_dump($posts));
- ?>
- <!-- Print post, name and comment. -->
+?>
+ <!-- print post, name and comment. -->
  <h1>The gallery</h1>
  <div class="gallery-page">
     <?php foreach ($posts as $post): ?>
@@ -44,18 +44,34 @@
             </div>
         <img style="width: 300px; height: 300px;" class="gallery-pics" src="image/post/<?php echo $post['post'];?>" alt="photoify">
         <p><b><?php echo $post['first_name'].' '. $post['last_name']?></b><?php echo ': '. $post['content'];?></p>
+<?php
+// select likes from database
+        $statement = $pdo->prepare(
+            'SELECT * FROM likes
+            WHERE post_id = :post_id
+            AND user_id = :user_id');
+            $statement->bindParam(':post_id', $post['post_id'], PDO::PARAM_INT);
+            $statement->bindParam(':user_id', $_SESSION['user']['user_id'], PDO::PARAM_INT);
+            $statement->execute();
+            $alreadyLiked = $statement->fetch(PDO::FETCH_ASSOC); ?>
         <form class="like-post" action="app/likes/like.php" method="post">
-            <p>
-              <button type="submit" name="like" class="like">LIKE</button>
-              <p>Liked by: 0</p>
+            <!-- change button if the post is liked by the user or not -->
+            <p><?php if($alreadyLiked):?>
+                <button type="submit" name="like" class="like">LIKED</button>
+           <?php else: ?>
+               <button type="submit" name="like" class="like">LIKE</button>
+           <?php endif; ?>
+           <br>
+            <p>Liked by: <?php echo $post['like']; ?></p>
            </p>
-           <input type="hidden" value="<?php echo $post['post_id'];?>" name="post_id" id=post_id>
+            <input type="hidden" value="<?php echo $post['post_id'];?>" name="post_id" id=post_id>
         </form>
         <!-- <a href="app/likes/like.php?type=like&post_id=<?php echo $post['post_id'];?>">0</a> -->
         <br>
         <br>
     </div>
+    <!-- end post -->
         <?php endforeach; ?>
     </div>
-</div>
+    <!-- end gallery-page -->
 </article>
