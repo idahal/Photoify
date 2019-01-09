24b45c124d2b4a69e32b93d87232978b61e5c7d5
Ida Hallner <bee_ida@hotmail.com>
2019-01-09 17:45:40 +0100

    add footer and icon

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,10 @@
+<div class="footer">
+    <li><i class="fas fa-home fa-lg"></i></li>
+    <li><i class="far fa-image fa-lg"></i></li>
+    <li><h4>Photoify</h4></li>
+    <li><i class="far fa-user fa-lg"></i></li>
+    <li><i class="fas fa-search fa-lg"></i></li>
+</div>
 </div><!-- /container -->
 
 <script src="/assets/scripts/main.js"></script>
