f8ceb4a24ac4f82c7408f1058ff92f00fc6462a2
Ida Hallner <bee_ida@hotmail.com>
2019-01-10 16:59:47 +0100

    split up css stylesheets

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,15 @@
-<div class="footer">
+</div><!-- /container -->
+<div class="footer-mobile">
     <li><i class="fas fa-home fa-lg"></i></li>
     <li><i class="far fa-image fa-lg"></i></li>
     <li><h4>Photoify</h4></li>
     <li><i class="far fa-user fa-lg"></i></li>
     <li><i class="fas fa-search fa-lg"></i></li>
 </div>
-</div><!-- /container -->
+
+<div class="footer-desktop">
+    <h4>Photoify</h4>
+</div>
 
 <script src="/assets/scripts/main.js"></script>
 </body>
