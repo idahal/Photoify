289a32ae0cfa936ec1a8da7dddd9fe5b205ca736
Ida Hallner <bee_ida@hotmail.com>
2019-01-11 16:38:43 +0100

    change color on icons

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,18 @@
 </div><!-- /container -->
 <div class="footer-mobile">
-    <li><i class="fas fa-home fa-lg"></i></li>
-    <li><i class="far fa-image fa-lg"></i></li>
+
+    <li>
+        <a href="index.php"><i style="color:black;" class="fas fa-home fa-lg"></i></li></a>
+    <li>
+        <a href="upload_photo.php"><i style="color:black;" class="far fa-image fa-lg"></i></a>
+    </li>
     <li><h4>Photoify</h4></li>
-    <li><i class="far fa-user fa-lg"></i></li>
-    <li><i class="fas fa-search fa-lg"></i></li>
+    <li>
+        <a href="mypage.php"><i style="color:black;" class="far fa-user fa-lg"></i></a>
+    </li>
+    <li>
+        <a href="myaccount.php"><i style="color:black;" class="far fa-edit fa-lg"></i></a>
+    </li>
 </div>
 
 <div class="footer-desktop">
@@ -12,5 +20,6 @@
 </div>
 
 <script src="/assets/scripts/main.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.6.3/js/all.js" integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1" crossorigin="anonymous"></script>
 </body>
 </html>
