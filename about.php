318793803ef8380852e560a9a0d6f70ad0b1f34b
Ida Hallner <bee_ida@hotmail.com>
2019-01-16 11:19:03 +0100

    small changes

--- about.php
+++ about.php
@@ -1,8 +1,12 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article>
+<article class="create-account">
     <h1>About</h1>
-    <p>This is the about page.</p>
+    <p>Photoify is a channel for those who like to share and view amazing photos.</p>
+    <br>
+<form action="app/users/login.php" method="post">
+    <b>Do not hesitate, <a href="/signup.php">sign up today for free</a></b>
+</form>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
