520c81cbb7867010a75709916b72af6296855f03
Ida Hallner <bee_ida@hotmail.com>
2019-02-04 11:43:44 +0100

    cs fixer

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -10,8 +10,8 @@ if (!isset($_SESSION['user'])) {
     $_SESSION['errors'] = $errors;
     redirect("/");
 } else {
-        $user = $_SESSION['user'];
-    }
+    $user = $_SESSION['user'];
+}
 
 //  delete posts in the database.
 if (!isset($_POST['post_id'])) {
@@ -19,36 +19,36 @@ if (!isset($_POST['post_id'])) {
     $_SESSION['errors'] = $errors;
     redirect("/");
 } else {
-        $id = $_SESSION['user']['user_id'];
-        $postId = $_POST['post_id'];
+    $id = $_SESSION['user']['user_id'];
+    $postId = $_POST['post_id'];
 
-        $statement = $pdo->prepare(
+    $statement = $pdo->prepare(
         'DELETE FROM posts
         WHERE post_id = :post_id
         AND user_id = :user_id'
     );
 
 
-        // binds variables to parameteres for insert statement
-        $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
-        $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
-        $statement->execute();
+    // binds variables to parameteres for insert statement
+    $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
+    $statement->execute();
 
-        //delete likes one the deleted post in database
-        $statement = $pdo->prepare(
+    //delete likes one the deleted post in database
+    $statement = $pdo->prepare(
          'DELETE FROM likes
          WHERE post_id = :post_id
          AND user_id = :user_id'
      );
 
-        $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
-        $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
-        $statement->execute();
+    $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
+    $statement->execute();
 
-        if (!$statement) {
-            die(var_dump($pdo->errorInfo()));
-        }
-        redirect('/mypage.php');
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
     }
+    redirect('/mypage.php');
+}
 
 redirect('/');
